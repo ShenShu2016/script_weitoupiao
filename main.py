d03f4df49937a86d20f2710ae84d6ca3012af4a4
Shen <shushen2013@gmail.com>
2021-05-31 02:56:15 -0400

    ipynotebook done

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from time import sleep
+
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 
 wd=webdriver.Chrome(r'chromedriver.exe')
+
+wd.get('https://www.baidu.com/')
+
+input_=wd.find_element_by_id('kw')
+
+input_.send_keys('shushengacademy')
+
+search_=wd.find_element_by_id('su')
+
+ActionChains(wd).move_to_element(search_).click(search_).perform()
+
+login=wd.find_element_by_class_name('register-btn')
+sleep(10)
+
+wd.find_element_by_tag_name('td')
+wd.find_element_by_link_text()
+wd.find_element_by_tag_name()
+wd.implicitly_wait(15)
